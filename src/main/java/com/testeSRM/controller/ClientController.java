3cfb03d3aeacf60cfce9a82d3f5d5d9825239c05
Yohanes26 <yohaneslopes@gmail.com>
2018-11-16 13:43:41 -0200

    Enable Cors Origin para acesso de outro dominio

--- src/main/java/com/testeSRM/controller/ClientController.java
+++ src/main/java/com/testeSRM/controller/ClientController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +21,14 @@ public class ClientController {
 	@Autowired
 	private ClientService clientService;
 	
+	@CrossOrigin(origins = "https://teste-srm.herokuapp.com")
 	@GetMapping
 	@ResponseBody
 	public List<ClientVO> findAll(){
 		return clientService.findAll();
 	}
-	
+
+	@CrossOrigin(origins = "https://teste-srm.herokuapp.com")
 	@PostMapping(path = "/save", consumes = "application/json", produces = "application/json")
 	public void saveClient(@RequestBody ClientVO client){
 		clientService.saveClient(client);

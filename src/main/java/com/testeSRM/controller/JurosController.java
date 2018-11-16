dc2cc2b0a15dbaf28f2a5f23bd6bc02110777e7a
Yohanes26 <yohaneslopes@gmail.com>
2018-11-16 14:34:38 -0200

    Inclusao da Annotation @CrossOrigin para passar pelo cors no controller de juros

--- src/main/java/com/testeSRM/controller/JurosController.java
+++ src/main/java/com/testeSRM/controller/JurosController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,12 +20,14 @@ public class JurosController {
 	@Autowired
 	private JurosService jurosService;
 	
+	@CrossOrigin(origins = "https://teste-srm.herokuapp.com")
 	@GetMapping
 	@ResponseBody
 	public List<JurosVO> findAll(){
 		return jurosService.findAll();
 	}
 	
+	@CrossOrigin(origins = "https://teste-srm.herokuapp.com")
 	@PostMapping(path = "/save", consumes = "application/json", produces = "application/json")
 	public void saveJuros(@RequestBody JurosVO juros){
 		jurosService.saveJuros(juros);
